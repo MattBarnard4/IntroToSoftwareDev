22e96a13832d7678e247fbf0d85abb79af46931a
luke-j-davis <luke.j.davis@student.uts.edu.au>
2026-03-11 18:56:58 +1100

    ADDED HTML and CSS
    and JS useless luke work

---
+++ register.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("button");
+const firsttitle = document.querySelector(".firsttitle");
+
+btn.addEventListener("click", function() {
+    firsttitle.style.color = "green";
+})

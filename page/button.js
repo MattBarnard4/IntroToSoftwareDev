63a3b849e4d8615ee3d8d695ee1f0952defe3440
NoahDean <106721327+NoahDean@users.noreply.github.com>
2026-03-18 18:29:36 +1100

    Week5 initial commit

--- page/button.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("change-color-button");
-const firstTitle = document.getElementById("first-title");
-
-button.addEventListener("click", () => {
-    firstTitle.style.color = "yellow";
-})
